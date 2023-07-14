7799bfb39d0ce515e5d452dd954c41ceb4e6d130
Christian Santana <christiansantana2110@gmail.com>
2023-07-14 13:49:34 -0600

    week-1-assessment

---
+++ gitInfo.js
@@ -0,0 +1,77 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitAddDefinition = 'is a system used in software that track changes in code in computer files'
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = 'Github is a website that allows software developers to manage and store their developments with git'
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = 'git init actually creates the new repository'
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = 'git clone makes a copy of a repository '
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = 'git status shows you the changes made to a repository and what is stored'
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = 'git add will add any changes to the repository'
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitCommitDefinition = 'git commit lets you add a message or leave a note on the repository. It can keep anyone updated or let them know why and what they did to the repository.'
+let gitCommitCode = 'git commit -m " "'
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = 'git push lets you update any repository created, you wouldnt have to use the comman git innit.'
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysisOfRequirements = 'The second step is when developer analyze and see what is required for the features that want to be added'
+let design = 'The third step is to figure out the code for the design and features that is wanted '
+let testingIntegration = 'The fourth step is to test the codes to see if it does the proper funtion or design that was wanted.'
+let implementation = 'The fifth step is to integrate the system into a work flow'
+let maintain = 'The sixth step is to keep improving the system, whether is hardware or system updates'
